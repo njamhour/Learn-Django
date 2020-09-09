e709a4090fff02925ebbe2bc02f04481797c1428
unknown <njjamhour@hotmail.com>
2020-09-09 16:27:14 -0300

    JSON CBV

--- restapi/updates/views.py
+++ restapi/updates/views.py
@@ -1,6 +1,9 @@
-# import json # Como era feito de forma anterior
+import json # Como era feito de forma anterior
+
+from restapi.mixins import JsonResponseMixin
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
+from django.views.generic import View
 
 from .models import Update
 # def detail_view(request):
@@ -19,3 +22,25 @@ def json_example_view(request):
     return JsonResponse(data)
     # json_data = json.dumps(data) # Como era feito da forma anterior
     # return HttpResponse(json_data, content_type='application/json') # Como era feito da forma anterior
+
+class JsonCBV(View):
+   def get(self, request, *args, **kwargs):
+    '''
+    URI - REST API
+    GET - Retrieve
+    '''
+    data = {
+        "count": 1000,
+        "content": "Some new content"
+    }
+    return JsonResponse(data)
+
+
+
+class JsonCBV2(JsonResponseMixin, View):
+   def get(self, request, *args, **kwargs):
+    data = {
+        "count": 1000,
+        "content": "Some new content"
+    }
+    return self.render_to_json_response(data)
