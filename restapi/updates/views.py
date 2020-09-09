19294bda7ea2f0c62b4ae5913728e0b78a536734
unknown <njjamhour@hotmail.com>
2020-09-09 16:12:36 -0300

    HttpResponse with Json Data

--- restapi/updates/views.py
+++ restapi/updates/views.py
@@ -1,3 +1,21 @@
+# import json # Como era feito de forma anterior
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Update
+# def detail_view(request):
+    # return render() # return JSON -> JS Object Notation
+    # return HttpResponse(get_template().render({}))
+
+def json_example_view(request):
+    '''
+    URI - REST API
+    GET - Retrieve
+    '''
+    data = {
+        "count": 1000,
+        "content": "Some new content"
+    }
+    return JsonResponse(data)
+    # json_data = json.dumps(data) # Como era feito da forma anterior
+    # return HttpResponse(json_data, content_type='application/json') # Como era feito da forma anterior
