b20f7eff95745734875ac44365b79b788f93d84b
unknown <njjamhour@hotmail.com>
2020-09-09 20:49:14 -0300

    Pratical Api Module

---
+++ restapi/api/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from updates.models import Update as UpdateModel
+
+class UpdateModelDetailAPIView():
+    def get(self, request, *args, **kwargs):
+        
+    
+    def post(self, request, *args, **kwargs):
+        
+        
+    def put(self, request, *args, **kwargs):
+        
+    
+    def delete(self, request, *args, **kwargs):
